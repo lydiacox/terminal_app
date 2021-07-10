6914302268d55d4a2de73deb661a4a8753355ae2
lydiacox <coxlydia01@gmail.com>
2021-07-10 12:11:40 +1000

    first commit

---
+++ challenge.py
@@ -0,0 +1,5 @@
+import emoji
+import os
+
+os.system('clear')
+print(emoji.emojize('You nailed it :thumbs_up:'))
---
+++ example.py
@@ -0,0 +1,9 @@
+import cowsay
+
+cowsay.cow("You're an excellent coder!")
+
+print(cowsay.get_output_string('trex', 'You have nice hair!'))
+
+cowsay.dragon("Slay, Queen!")
+
+cowsay.stimpy("Nice bum")
