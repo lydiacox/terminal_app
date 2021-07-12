21dc24a8d966f3ed0c7cc2b81b99d6a5ed4d10e6
lydiacox <coxlydia01@gmail.com>
2021-07-12 16:43:27 +1000

    draw first snowman

---
+++ ascii_art.py
@@ -0,0 +1,44 @@
+snow_scenes = (
+# first snowman scene, complete snowman
+    """
+    
+         *         _________
+           *        |     |       *
+      *             |     |    *
+                    |     |
+              *     -------          *
+                   /       \\
+         *        (  *   *  ) *    *
+                  (    ---> )   *
+                  (   `---' )
+           *       \\_______/        *
+       *          /======88=\\
+                 /       ||  \\   *
+                |      * ||   |
+         >------|      * ||   |------<
+        *       |      * ||   |      *
+                 \\_______##__/  *
+          *     /             \\
+      *        /               \\   *
+              /                 \\
+             (                   )
+             (                   )  *
+          *  (                   )
+              \\                 /
+       *       \\_______________/
+    """,
+# second snowman scene, no hat
+
+# third snowman scene, no scarf
+
+# fourth snowman scene, no eyes
+
+# fifth snowman scene, no nose
+
+# sixth snowman scene, no head
+
+# seventh snowman scene, no arms
+)
+
+for scene in snow_scenes:
+    print(scene)
