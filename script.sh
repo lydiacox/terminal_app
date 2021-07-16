f06be3d205112b086b24e6dd609211ad96a6ec57
lydiacox <coxlydia01@gmail.com>
2021-07-16 14:47:04 +1000

    modify script

--- script.sh
+++ script.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-pip3 install -r requirements.txt
-python3 challenge.py
+if [[ $1 == 'about' ]]
+then
+    cat ABOUT.txt
+    exit 0
+fi
+
+if ! [[ -x "$(command -v python)" || -x "$(command -v python3)" ]]
+then
+    echo "Error: This program runs on Python, but it looks like Python is not installed. To install Python, check out https://installpython3.com/" >&2
+    exit 1
+elif python3 --version | grep -q '^Python 3\.'
+then
+    echo "You have Python 3, let's play!"
+    python3 snowperson.py
+elif python --version 2>&1 | grep -q '^Python 3\.'
+then
+    echo "You have Python 3, let's play!"
+    python snowperson.py
+else
+    echo "Error: This program runs on Python 3, but it looks like Python 2 or older is installed. To install Python, check out https://installpython3.com/" >&2
+    exit 1
+fi
